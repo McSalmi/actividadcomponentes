bdfdd3eddfae4c82f5a1993049282603ce2231d6
mcSalmi <mcSalmi>
2023-04-12 01:44:07 -0600

    pagina

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Promotion from './components/Promotions';
+import Producto from './components/Producto';
+import ShoppingCart from './components/ShoppingCart';
+import Checkout from './components/Checkout';
+import './components/Estilo.css';
 
-function App() {
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const products = [
+  { id: 1, name: 'Product 1', price: 9.99 },
+  { id: 2, name: 'Product 2', price: 14.99 },
+  { id: 3, name: 'Product 3', price: 19.99 },
+];
+
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    const newProduct = new Product(product.name, product.price);
+    setCartItems([...cartItems, newProduct]);
+  };
+
+  const handleRemoveFromCart = (productId) => {
+    setCartItems(cartItems.filter((item) => item.id !== productId));
+  };
+
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Promotion />
+      <div className="main-content">
+        <Producto products={products} onAddToCart={handleAddToCart} />
+        <ShoppingCart items={cartItems} onRemoveFromCart={handleRemoveFromCart} />
+        <Checkout cartItems={cartItems} total={totalPrice} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
